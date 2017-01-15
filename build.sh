359cf689029b96aa7169ceb02e4b83551e574aa2
jmarsalis <jmarsalis@gmail.com>
2017-01-15 10:09:09 -0600

    added build script and travis file

---
+++ build.sh
@@ -0,0 +1,21 @@
+#!/usr/bin/env bash
+
+# exit if any command fails
+set -e
+
+# the location of the output of the build output (must be an absolute path)
+artifactsFolder="/tmp/build-artifacts/bcca"
+
+# remove the drop location if it exists
+if [ -d $artifactsFolder ]; then  
+  rm -R $artifactsFolder
+fi
+
+# get the revision number for the version from the travis job
+revision=${TRAVIS_JOB_ID:=1}  
+revision=$(printf "%04d" $revision)
+
+# restore and build the application
+dotnet restore
+#dotnet build -c Release
+dotnet publish ./project.json -c Release -o $artifactsFolder --version-suffix=$revision
